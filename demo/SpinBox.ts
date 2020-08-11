be69f9e5354e7cbd298c22c4c75e9f4c9cf8fe1b
Jan Miksovsky <jmiksovsky@salesforce.com>
2020-08-11 10:32:26 -0700

    Move decorators to src folder.

--- demo/SpinBox.ts
+++ demo/SpinBox.ts
@@ -1,6 +1,6 @@
 import { ids } from "elix/src/base/internal.js";
 import ReactiveElement from "elix/src/core/ReactiveElement.js";
-import { api } from "./decorators";
+import { api } from "../src/decorators";
 
 export default class SpinBox extends ReactiveElement {
   @api()
